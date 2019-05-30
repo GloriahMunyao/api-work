aa4ad825633f251956015ead5c066176406bde22
GloriahMunyao <33954193+GloriahMunyao@users.noreply.github.com>
2019-05-30 12:36:33 +0300

    Add files via upload

---
+++ api-work/rats/screencaster.php
@@ -0,0 +1,14 @@
+<?php
+ function takes_array($input)
+{
+ echo "$input[0] + $input[1] = ", $input[0]+$input[1];
+}
+function f($x='1', $y='3'){
+   $output = $x*$y;
+   echo $output;
+}
+f($y='1',$x='3');
+
+
+
+?>  
